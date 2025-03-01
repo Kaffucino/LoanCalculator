1f888282613d58839c9616b6f10b305f0c4ab888
Filip Starcevic <83342986+Kaffucino@users.noreply.github.com>
2025-03-01 21:16:39 +0100

    Initial commit

---
+++ src/test/java/com/example/LoadCalculator/LoadCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoadCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
